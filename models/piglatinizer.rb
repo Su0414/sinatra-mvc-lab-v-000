02572689f6113bf48b11118fc9de60b909405511
Suchitra Naik <susanaik@gmail.com>
2018-06-07 17:33:01 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,10 +1,13 @@
-class PigLatinizer 
+class PigLatinizer
   attr_accessor :words
-  
-  def initialize(words) 
-    @words = words 
-  end 
 
-  def piglatinize()
-  end 
-end 
+  def initialize(words)
+    @words = words
+  end
+
+  def piglatinize(str_words)
+    arr = []
+    arr << str_words.split(" ")
+
+  end
+end
