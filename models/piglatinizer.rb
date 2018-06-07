e96800ad45c74bfbbd86d66b27f589a458ae52f8
Suchitra Naik <susanaik@gmail.com>
2018-06-07 20:01:24 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -16,17 +16,15 @@ class PigLatinizer
       word.each_char.with_index(1).map { |val,index|
         if 'aeiouAEIOU'.include?val
           i = index - 1
-          break;
+          str1 = word.slice(0,i)
+          str2 = word.slice(i)
+          str3 = word.slice(i, word.length)
+          if(i == 0)
+            str = "way"
+          end
+          str4 =  str3 + str1 + str
         end
       }
-
-    str1 = word.slice(0,i)
-    str2 = word.slice(i)
-    str3 = word.slice(i, word.length)
-    if(i == 0)
-      str = "way"
-    end
-    str4 =  str3 + str1 + str
     arr << str4
 end
     arr.join(" ")
