410066e9e3ed2bf287e45012e3326300ca303dd9
Suchitra Naik <susanaik@gmail.com>
2018-06-07 18:04:24 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,17 +1,29 @@
 require 'pry'
 
 class PigLatinizer
-  attr_accessor :words
+  attr_accessor :text
 
-  def initialize(words)
-    @words = words
+  def initialize
+    @text = text
+  end
+
+  def number_of_words
+    words = text.split(" ")
   end
 
   def piglatinize(str_words)
     arr = []
-    arr << str_words.split(" ")
+    arr << number_of_words.split(" ")
+
     binding.pry
 
+    arr.each_with_index do |word, index|
+      alpha = word.split('')
+      if alpha.scan(/[aeiou]/)
+
+      end
+
+    end
 
   end
 end
